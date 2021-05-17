ca91bc9989c34d6e9c01f3bfa26e03a286330ca0
franciscocamellon <franciscocamellon@gmail.com>
2021-05-16 21:56:17 -0300

    lvdb user input

--- cmd/qad_lvdb_cmd.py
+++ cmd/qad_lvdb_cmd.py
@@ -30,9 +30,10 @@ from qgis.PyQt.QtGui import *
 from qgis.utils import iface
 from qgis.core import Qgis, QgsProject, QgsWkbTypes
 
-
+from .qad_lvdb_maptool import Qad_lvdb_maptool, Qad_lvdb_maptool_ModeEnum
 from .qad_generic_cmd import QadCommandClass
 from ..qad_textwindow import QadInputModeEnum, QadInputTypeEnum
+from ..qad_getpoint import QadGetPointDrawModeEnum
 from .. import qad_layer
 from ..qad_msg import QadMsg
 from ..qad_layer import getLayersByName, getCurrLayerEditable
@@ -65,6 +66,15 @@ class QadLVDBCommandClass(QadCommandClass):
         QadCommandClass.__init__(self, plugIn)
         self.iface = self.plugIn.iface
         self.targetLayer = 'LVDB-FP'
+        self.lvFuse = 0
+
+    def getPointMapTool(self, drawMode = QadGetPointDrawModeEnum.NONE):
+      if (self.plugIn is not None):
+         if self.PointMapTool is None:
+            self.PointMapTool = Qad_lvdb_maptool(self.plugIn)
+         return self.PointMapTool
+      else:
+         return None
 
     def isLoadedLayer(self):
         layer = getLayersByName(self.targetLayer)
@@ -91,43 +101,59 @@ class QadLVDBCommandClass(QadCommandClass):
     def isFeatureSelected(self):
         editable, layer = self.isEditable()
         if editable:
-            # layer = self.iface.activeLayer()
-            selectedFeature = [feature for feature in layer.getSelectedFeatures()]
-            if len(selectedFeature) == 1:
+            selectedFeature = [
+                feature for feature in layer.getSelectedFeatures()]
+            featLen = len(selectedFeature)
+            if featLen == 1:
                 return selectedFeature
-            elif len(selectedFeature) > 1:
-                self.showMsg(QadMsg.translate(
-                    "QAD", '\nPlease select only one feature from {}.\n'.format(layer.name())))
-            elif len(selectedFeature) == 0:
+            elif featLen == 0 or featLen > 1:
                 self.showMsg(QadMsg.translate(
                     "QAD", '\nPlease select one feature from {}.\n'.format(layer.name())))
 
     def getClosedLvQuantity(self):
         selectedFeature = self.isFeatureSelected()
-        
-        targetFields = ['lvf_1', 'lvf_2', 'lvf_3', 'lvf_4',
-                        'lvf_5', 'lvf_6', 'lvf_7', 'lvf_8', 'lvf_9', 'lvf_10']
         if selectedFeature:
             fields = selectedFeature[0].fields()
             for field in fields:
-                if field.name() in targetFields:
-                    closedLVQuantity = self.getClosedLV(selectedFeature[0][field.name()])
-                    
-                    self.showMsg(QadMsg.translate(
-                        "QAD", '\ncampo - {}\n'.format(closedLVQuantity)))
-        else:
-            self.showMsg(QadMsg.translate(
-                    "QAD", '\nPlease select one feature from\n'))
+                closed = self.getClosedLV(selectedFeature[0][field.name()])
+                if closed == 'CLOSED:':
+                    self.lvFuse += 1
+            return self.lvFuse
 
     def getClosedLV(self, attribute):
         try:
             if attribute:
-                print(attribute)
                 regex = re.compile(r'\bclosed:\b', re.IGNORECASE)
                 closedLV = regex.findall(attribute)
-            return closedLV
+            return closedLV[0]
         except:
             pass
+
+    # ============================================================================
+    # waitForNumOfLvFuse
+    # ============================================================================
+    def waitForNumOfLvFuse(self):      
+        # imposto il map tool
+        self.getPointMapTool().setMode(Qad_lvdb_maptool_ModeEnum.ASK_FOR_LV_FUSE_NUMBER)
+        self.getPointMapTool().gapType = self.gapType                        
+
+        keyWords = QadMsg.translate("Command_OFFSET", "All") + "/" + \
+                    QadMsg.translate("Command_OFFSET", "None")                
+        default = QadMsg.translate("Command_OFFSET", "None")
+        prompt = QadMsg.translate("Command_OFFSET", "Specify the number of LV fuse to draw [{0}] <{1}>: ").format(keyWords, unicode(default))
+
+        englishKeyWords = "All" + "/" + "None"
+        keyWords += "_" + englishKeyWords
+        # si appresta ad attendere un punto o enter o una parola chiave o un numero reale     
+        # msg, inputType, default, keyWords, nessun controllo
+        self.waitFor(prompt, \
+                    QadInputTypeEnum.POINT2D | QadInputTypeEnum.FLOAT | QadInputTypeEnum.KEYWORDS, \
+                    default, \
+                    keyWords, \
+                    QadInputModeEnum.NOT_ZERO | QadInputModeEnum.NOT_NEGATIVE)      
+        self.step = 1 
+
+
 
     def run(self):
         self.isLoadedLayer()
